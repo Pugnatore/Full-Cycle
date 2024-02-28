e3e932782c5b694cf52b5ba9401fab442a7c11a8
pugnatore <paulofmb@hotmail.com>
2024-02-28 16:25:46 +0000

    Add Clients to walletCore

---
+++ WalletCore/internal/entity/client.go
@@ -0,0 +1,62 @@
+package entity
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+type Client struct {
+	ID        string
+	Name      string
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func NewClient(name string, email string) (*Client, error) {
+	client := &Client{
+		ID:        uuid.New().String(),
+		Name:      name,
+		Email:     email,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err := client.Validate(name, email)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+
+}
+
+func (c *Client) Update(name string, email string) error {
+	c.Name = name
+	c.Email = email
+	c.UpdatedAt = time.Now()
+
+	err := c.Validate(name, email)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) Validate(name string, email string) error {
+	if name == "" {
+		return errors.New("name is required")
+	}
+	if email == "" {
+		return errors.New("email is required")
+	}
+
+	c.Name = name
+	c.Email = email
+
+	return nil
+}
